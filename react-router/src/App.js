5bc77141853f41fee3559f7e5b0e66222f0fa3ff
LukasBursa <bursalukas@gmail.com>
2023-03-12 23:42:17 +0100

    pridani komponent

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-function App() {
+import Home from "./pages/Home"
+import Movies from './pages/Movies'
+import Serials from './pages/Serials'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/movies" element={ <Movies /> } />
+        <Route path="/serials" element={ <Serials /> } />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
